5d0b3c70fbd9179baaf71b824b2adb7e7a486794
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-02-04 02:03:14 +0600

    login design

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            login
-        </div>
+      <div className="flex h-screen items-center">
+        <form className="w-1/2 mx-auto border h-fit p-3">
+          <h1 className="text-center font-bold text-xl">Login</h1>
+          <div className="name text-center">
+            <label htmlFor="">Enter Your Name:</label>
+            <br />
+            <input type="text" name="name" className="border" />
+          </div>
+          <div className="email text-center">
+            <label htmlFor="">Enter Your Email:</label>
+            <br />
+            <input type="email" name="email" className="border" />
+          </div>
+          <div className="submit text-center my-3">
+            <button type="submit" className="border p-2 rounded">
+              Submit
+            </button>
+          </div>
+          <div className="register text-center">
+            <Link className="border p-2 rounded" to="/register">
+              Register
+            </Link>
+          </div>
+        </form>
+      </div>
     );
 };
 
