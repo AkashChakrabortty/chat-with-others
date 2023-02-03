249f8ded0da577f7b429b7e366b1b572f714014a
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-02-04 02:07:53 +0600

    register design

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,15 +6,15 @@ const Login = () => {
       <div className="flex h-screen items-center">
         <form className="w-1/2 mx-auto border h-fit p-3">
           <h1 className="text-center font-bold text-xl">Login</h1>
-          <div className="name text-center">
-            <label htmlFor="">Enter Your Name:</label>
-            <br />
-            <input type="text" name="name" className="border" />
-          </div>
           <div className="email text-center">
             <label htmlFor="">Enter Your Email:</label>
             <br />
             <input type="email" name="email" className="border" />
+          </div>
+          <div className="password text-center">
+            <label htmlFor="">Enter Your Password:</label>
+            <br />
+            <input type="text" name="password" className="border" />
           </div>
           <div className="submit text-center my-3">
             <button type="submit" className="border p-2 rounded">
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,7 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
-    return <div>Register</div>;
+    return (
+      <div className="flex h-screen items-center">
+        <form className="w-1/2 mx-auto border h-fit p-3">
+          <h1 className="text-center font-bold text-xl">Register</h1>
+          <div className="name text-center">
+            <label htmlFor="">Enter Your Name:</label>
+            <br />
+            <input type="text" name="name" className="border" />
+          </div>
+          <div className="email text-center">
+            <label htmlFor="">Enter Your Email:</label>
+            <br />
+            <input type="email" name="email" className="border" />
+          </div>
+          <div className="password text-center">
+            <label htmlFor="">Enter Your Password:</label>
+            <br />
+            <input type="text" name="password" className="border" />
+          </div>
+          <div className="submit text-center my-3">
+            <button type="submit" className="border p-2 rounded">
+              Submit
+            </button>
+          </div>
+          <div className="register text-center">
+            <Link className="border p-2 rounded" to="/">
+              Login
+            </Link>
+          </div>
+        </form>
+      </div>
+    );
 };
 
 export default Register;
