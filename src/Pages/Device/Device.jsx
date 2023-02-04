d6eff18a0212a4493ebb76b79b035ef2e4a58e18
AkashChakrabortty <akashchakrabortty@gmail.com>
2023-02-04 15:13:52 +0600

    logout

--- src/Pages/Device/Device.jsx
+++ src/Pages/Device/Device.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { userInfo } from '../../context/AuthProvider';
 
 const Device = () => {
+   const { logout } = useContext(userInfo);
+
+    const handleLogout = () => {
+      logout();
+    };
+    
     return (
       <div className="flex h-screen items-center">
         <div className="w-10/12 mx-auto border h-fit p-3">
+          <button
+            type="submit"
+            className="border p-2 rounded"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
           <table className="table-auto w-full text-center">
             <thead className="border">
               <tr>
                 <th>Browser</th>
                 <th>OS</th>
                 <th>Date</th>
-                <th>Action</th>
               </tr>
             </thead>
             <tbody>
@@ -18,33 +31,18 @@ const Device = () => {
                 <td>Chrome</td>
                 <td>Windows 10</td>
                 <td>4/12/12</td>
-                <td>
-                  <button type="submit" className="border p-2 rounded">
-                    Remove
-                  </button>
-                </td>
               </tr>
 
               <tr>
                 <td>Chrome</td>
                 <td>Windows 10</td>
                 <td>4/12/12</td>
-                <td>
-                  <button type="submit" className="border p-2 rounded">
-                    Remove
-                  </button>
-                </td>
               </tr>
 
               <tr>
                 <td>Chrome</td>
                 <td>Windows 10</td>
                 <td>4/12/12</td>
-                <td>
-                  <button type="submit" className="border p-2 rounded">
-                    Remove
-                  </button>
-                </td>
               </tr>
             </tbody>
           </table>
